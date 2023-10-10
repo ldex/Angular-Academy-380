dcd22ea761323190114a63919407f6ba9290f3ec
ldex <laurent.duveau+git@gmail.com>
2023-10-10 15:25:29 -0400

    http request and observable

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, delay, tap } from 'rxjs';
+import { Product } from '../models/product.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = 'https://storerestservice.azurewebsites.net/api/products/';
+  products$: Observable<Product[]>;
+
+  constructor(private http: HttpClient) {
+    this.initProducts();
+  }
+
+  initProducts() {
+    this.products$ = this
+                      .http
+                      .get<Product[]>(this.baseUrl)
+                      .pipe(
+                        delay(1500), // for the demo...
+                        tap(console.table)
+                      );
+  }
+}
